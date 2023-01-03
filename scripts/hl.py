c2d14bc1d4558c29fd5b536d8203a66c7dbddd93
ilya-mezentsev <ism20122@yandex.ru>
2023-01-03 17:57:52 +0300

    нагрузка по шагам

--- scripts/hl.py
+++ scripts/hl.py
@@ -1,11 +1,18 @@
 import random
 import uuid
 from abc import abstractmethod
+from typing import (
+    Sequence,
+    Mapping,
+    Optional,
+    Tuple,
+)
 
 from locust import (
     HttpUser,
     task,
-    between,
+    constant,
+    LoadTestShape,
 )
 from locust.clients import HttpSession
 
@@ -43,7 +50,7 @@ class UserMixin:
 
 
 class Role1User(HttpUser, UserMixin):
-    wait_time = between(0.1, 0.2)
+    wait_time = constant(1)
 
     @task
     def do_task(self) -> None:
@@ -54,7 +61,7 @@ class Role1User(HttpUser, UserMixin):
 
 
 class Role2User(HttpUser, UserMixin):
-    wait_time = between(0.1, 0.2)
+    wait_time = constant(1)
 
     @task
     def do_task(self) -> None:
@@ -65,7 +72,7 @@ class Role2User(HttpUser, UserMixin):
 
 
 class Role3User(HttpUser, UserMixin):
-    wait_time = between(0.1, 0.2)
+    wait_time = constant(1)
 
     @task
     def do_task(self) -> None:
@@ -73,3 +80,35 @@ class Role3User(HttpUser, UserMixin):
 
     def get_account_id(self) -> str:
         return ROLE_3_ID
+
+
+class StagesShape(LoadTestShape):
+    """
+    A simple load test shape class that has different user and spawn_rate at
+    different stages.
+    Keyword arguments:
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+
+    stages: Sequence[Mapping[str, int]] = [
+        {'duration': 30, 'users': 30, 'spawn_rate': 10},
+        {'duration': 210, 'users': 90, 'spawn_rate': 10},
+        {'duration': 450, 'users': 180, 'spawn_rate': 10},
+        {'duration': 630, 'users': 90, 'spawn_rate': 10},
+        {'duration': 660, 'users': 30, 'spawn_rate': 5},
+    ]
+    stop_at_end = True
+
+    def tick(self) -> Optional[Tuple[int, int]]:
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage['duration']:
+                tick_data = (stage['users'], stage['spawn_rate'])
+                return tick_data
+
+        return None
