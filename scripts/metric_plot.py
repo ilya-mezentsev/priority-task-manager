8685716d5343adaf3aa84ec60c40f454b03b9132
ilya-mezentsev <ism20122@yandex.ru>
2023-01-17 23:38:11 +0300

    доработки по скрипту для рисования сравнительных графиков; заметки по метрикам

--- scripts/metric_plot.py
+++ scripts/metric_plot.py
@@ -2,13 +2,21 @@
 Скрипт для рисования графиков.
 В частности, для удобного сравнения графиков
 """
+import os
 import sys
 
 import matplotlib.pyplot as plt
 
 
-assert len(sys.argv) > 2, f'Missed data files'
+assert len(sys.argv) == 3, f'Script can process exactly two arguments; first - is metric before, second - is after'
 
+before_title = os.environ.get('BEFORE_TITLE') or 'Before'
+after_title = os.environ.get('AFTER_TITLE') or 'After'
+
+labels = {
+    before_title,
+    after_title,
+}
 time_points_list: list[list[float]] = []
 for file in sys.argv[1:]:
     with open(file, 'r') as f:
@@ -20,10 +28,14 @@ for time_points in time_points_list:
     plt.plot(
         x_points,
         list(map(float, time_points[:min_points_count])),
+        label=labels.pop(),
     )
 
 plt.ylabel('Время ожидания задачи взятия в работу, [с]')
 plt.xlabel('Время, [с]')
 
 plt.grid()
+plt.title(os.environ.get('TITLE') or f'{before_title} vs {after_title}')
+plt.legend()
+
 plt.show()
